8ee548c91609f53d0a01851f5377194edd895171
alder <fultona5@msu.edu>
2020-11-03 12:29:45 -0600

    Removed obselete comments, created the main function and dataframe, created one of the analysis functions. Reworked structure of the code.

--- scripts/graphs/executor.py
+++ scripts/graphs/executor.py
@@ -4,50 +4,47 @@
 Master code file.
 """
 
-__author__ = "Scott Teresi"
+__author__ = "Alder Fulton"
 
 import argparse
 import os
 import logging
 import coloredlogs
+import pandas as pd
 
 # from module import module
 
 # TODO import your functions and classes here to invoke in process()
 
 
-def process():
-    raise NotImplementedError
+def process(folder):
+    '''
+    Args:
+        folder (str): path to the folder in which the modules are stored.
+    '''
+    df_list = []    #Initailize list of dataframes.
+    df_names = []   #Initialize list of the names associated with each data frame.
+    genes = []      #Initialize list of unique genes
+    for files in os.listdir(folder):
+        if(files.endswith(".tsv")):
+            df_list.append(pd.value_counts(pd.read_csv(folder+files,names = [files[0:-4]]).iloc[:,0]))
+            df_names.append(files[0:-4])
+            genes = set(list(genes) + list(df_list[-1].index))
+    total_df = pd.DataFrame(index = genes)
+    for df in df_list:
+        total_df = total_df.join(df)
+    total_df = total_df.fillna(0)
+    print(total_df)
+    print(genes_repeated(5,total_df))
 
-    # In the short term we want to understand what genes are repeated within a
-    # given module. We also want to know how many genes are repeated between
-    # modules
 
-    # In the longer term we want to know how many genes are repeated within a
-    # DEG library comparison. We also want to know how many genes are repeated
-    # between library comparisons
-
-    # In the even longer term we want to know how many genes are shared between
-    # the network and between the full body of DEGs. Venn diagram?
-
-    # Define your instances here.
-    # And use them below and we will invoke all of the graphing commands in
-    # process()
-
-    # TODO
-    # Iterate over the module dir here, declare a function
-    # Call the constructor (ModuleData)in the iterator
-    # modulesObject = ModuleData(path_to_modules)
-    # Add the module object to some sort of container or iterable
-    # Return a list of modules??? Then every following graphing function then
-    # has to accept a list of modules or a single module.... Is that what we
-    # want?
-    # FInish the iterator
-
-    degsObject = DegData()
-
+def genes_repeated(N,df):
+    #Returns a list of the genes repeated at least N times.
+    return df[df.sum(axis=1)>=N].index
+#Write function to save it TODO
 
 if __name__ == "__main__":
+    '''
     parser = argparse.ArgumentParser(description="")
     path_main = os.path.abspath(__file__)
 
@@ -62,5 +59,6 @@ if __name__ == "__main__":
     logger = logging.getLogger(__name__)
     coloredlogs.install(level=log_level)
 
+    '''
     # Process
-    process()
+    process('/home/alder/research/Blueberry_Data/WGCNA_Data/modulecolors_AT/')
