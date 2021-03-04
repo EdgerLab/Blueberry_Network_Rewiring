fed494c7f1166802c81d1a274029740db39122fd
alder <fultona5@msu.edu>
2021-03-04 13:47:51 -0600

    Added preliminary readme information, cleaned up the code in preperation for adding to the makefile and merging this branch with the master.

--- scripts/graphs/executor.py
+++ scripts/graphs/executor.py
@@ -24,12 +24,14 @@ def process(folder):
         folder (str): path to the folder in which the modules are stored.
     """
     total_df = import_dataframe(folder)
-    print(total_df)
-    print(genes_repeated(5, total_df))
-    print(genes_repeated_same_module(2,total_df))
+    # print(total_df)
+    # print(genes_repeated(5, total_df))
+    # print(genes_repeated_same_module(2, total_df))
     save(total_df)
 
+
 def import_dataframe(folder):
+    # This function makes a dataframe of all the genes and modules.
     """
     Args:
         folder (str): path to folder with multiple tsv files in it.
@@ -48,27 +50,35 @@ def import_dataframe(folder):
                 )
             )
             df_names.append(files[0:-4])
-            genes = set(list(genes) + list(df_list[-1].index))
+            genes = set(
+                list(genes) + list(df_list[-1].index)
+            )  # ensures there are no repeates.
     total_df = pd.DataFrame(index=genes)
     for df in df_list:
         total_df = total_df.join(df)
-    total_df = total_df.fillna(0)
+    total_df = total_df.fillna(0)  # Fills the nan values created by joining dfs.
     return total_df
 
 
 def uniques(df):
-    # Returns the dataframe so that each value that was more than one is set to
-    # one. Useful for determining overlap ocurring outside of the same module.
+    """
+    Returns the dataframe so that each value that was more than one is set to
+    one. Useful for determining overlap ocurring outside of the same module.
+    """
     df[df > 1] = 1
     return df
 
 
 def genes_repeated(N, df):
     # Returns a list of the genes repeated at least N times.
+    # This will also work on a df already run through uniques(df).
     return df[df.sum(axis=1) >= N].index
 
-def genes_repeated_same_module(N,df):
-    '''
+
+def genes_repeated_same_module(N, df):
+    # Gives a dictionary containing each gene and how many times it is repeated
+    # TODO make sure my description is correct..
+    """
     Args:
         N (int): the number of repetitions that you want to know about.
         df (dataframe): The dataframe you want to test the collumns of.
@@ -76,7 +86,7 @@ def genes_repeated_same_module(N,df):
         repeated (dataframe): A dataframe with the same collumns of df, dropped
             if there are no values in it, and contains a row of a list of each
             gene repeated that many times.
-    '''
+    """
     dictionary = {}
     for name in df.columns:
         mask = df[name] >= N
@@ -84,36 +94,45 @@ def genes_repeated_same_module(N,df):
             dictionary[name] = list(df[mask].index.values)
     return dictionary
 
-def create_histogram(sr,name):
+
+def create_histogram(sr, name):
+    # Creates a histogram of a single module.
     """
     Args:
         sr (pandas series): The series of the genes
     Outputs:
         fig (plt figure): Histogram of the number of ocurrences.
     """
-    sr = sr[sr!=0]
+    sr = sr[sr != 0]
     values = sr.value_counts().values
-    fig =  plt.bar(range(1,len(values)+1),values,align = 'edge')
+    fig = plt.bar(range(1, len(values) + 1), values, align="edge")
     plt.xlabel("# of Repetitions")
     plt.ylabel("# of Genes")
-    plt.yscale('log')
-    for i in range(1,len(values)+1):
-        plt.text(i,values[i-1]+.01,str(values[i-1]))
-    plt.savefig("hist_" + name, bbox_inches='tight', dpi=150)
+    plt.yscale("log")
+    for i in range(1, len(values) + 1):
+        plt.text(i, values[i - 1] + 0.01, str(values[i - 1]))
+    plt.savefig("hist_" + name, bbox_inches="tight", dpi=150)
     return fig
 
-def save(df,filename = 'results.csv'):
-    df.to_csv(filename,sep='\t')
+
+def save(df, filename="results.tsv"):
+    # Saves the results. TODO remove the file results.csv
+    df.to_csv(filename, sep="\t")
+
 
 def read(filename):
-    df = pd.read_csv(filename,sep = '\t')
-    df.set_index(df.columns[0],inplace=True)
-
+    """
+    Reads in the tsv files. Also is the main function that is currently edited.
+    """
+    df = pd.read_csv(filename, sep="\t")
+    df.set_index(df.columns[0], inplace=True)
 
     name = df.columns[0]
     for name in df.columns:
-        fig = create_histogram(df[name],name)
+        fig = create_histogram(df[name], name)
         plt.clf()
+
+
 if __name__ == "__main__":
     """
     parser = argparse.ArgumentParser(description="")
@@ -131,10 +150,20 @@ if __name__ == "__main__":
     coloredlogs.install(level=log_level)
 
     """
-    read_only = True
+    path = "/home/alder/research/Blueberry_Data/WGCNA_Data/modulecolors_AT_with_duplicates/"
+
+    read_only = False
     if read_only:
-        read('results.csv')
+        read("results.csv")
     else:
         # Process
-        process("/home/alder/research/Blueberry_Data/WGCNA_Data/modulecolors_AT_with_duplicates/")
-        #process("/home/alder/research/Blueberry_Data/WGCNA_Data/missing_modulecolors_BB/")
+        process(path)
+
+# TODO add this to the readme.
+"""
+Process creates the dataframe from the file location and then saves it in the
+working directory.
+Alternative functions for working with this dataframe are create_histogram,
+genes_repeated, genes_repeated_same_module, uniques, etc. For more information
+about these functions, or to use them, refere to file.
+"""
